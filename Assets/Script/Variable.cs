86961c479722d4eca50cbfafe376809f0ca08046
Soni <ciso20039@g.nihon-u.ac.jp>
2023-07-29 17:27:19 +0900

    playercontroll

---
+++ Assets/Script/Variable.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variable : MonoBehaviour
+{
+    public static int power = 1;
+    public static int enemyhp = 2;
+    public static int directiona = 1;
+    public static int directionb = 1;
+    public static int ra = 1;
+    public static int rb = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
