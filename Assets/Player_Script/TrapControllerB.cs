3afdb0e7381a179aa211efafb3e6d8f5aa34967d
ChoiMP <99106482+ChoiMP@users.noreply.github.com>
2023-07-30 03:12:54 +0900

    312

--- Assets/Player_Script/TrapControllerB.cs
+++ Assets/Player_Script/TrapControllerB.cs
@@ -9,25 +9,28 @@ public class TrapControllerB : MonoBehaviour
 
     private float puddle_timeb = 0;
     private int puddle_flagb = 1;
+    public GameObject particle;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
     void Update()
     {
-        if(hole_flagb == 2){
+        if (hole_flagb == 2)
+        {
             hole_timeb += Time.deltaTime;
-            if(hole_timeb > 3.0)
+            if (hole_timeb > 3.0)
             {
                 MovePlayerB.moveSpeedb = 5;
                 hole_flagb = 1;
                 hole_timeb = 0f;
             }
         }
-        if(puddle_flagb == 2){
+        if (puddle_flagb == 2)
+        {
             puddle_timeb += Time.deltaTime;
-            if(puddle_timeb > 3.0)
+            if (puddle_timeb > 3.0)
             {
                 MovePlayerB.moveSpeedb = 5;
                 puddle_flagb = 1;
@@ -38,15 +41,28 @@ public class TrapControllerB : MonoBehaviour
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "hole")
+        if (other.gameObject.tag == "hole")
         {
             MovePlayerB.moveSpeedb = 0;
             hole_flagb = 2;
         }
-        if(other.gameObject.tag == "puddle")
+        if (other.gameObject.tag == "puddle")
         {
             MovePlayerB.moveSpeedb = 2.5f;
             puddle_flagb = 2;
+            StartCoroutine(SlowParticle());
         }
+
+    }
+    IEnumerator SlowParticle()
+    {
+        GameObject go = Instantiate(particle);
+        for(int i=0;i<3;i++)
+        {
+            go.transform.position = transform.position;
+            yield return new WaitForSeconds(1f);
+        }
+        Destroy(go);
+        yield return null;
     }
 }
